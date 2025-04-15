e27bb6a9b0eaeaba3178d8c40f80a90f167c13f7
kiyohiroleo <kiyohiroleo@gmail.com>
2025-04-15 15:28:23 -0300

    astype

--- an.py
+++ an.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 #Ler o arquivo
 df = pd.read_csv("dataset1.csv", sep=";")
+
 #Mostra tudo
 pd.set_option('display.max_rows', None)
+
 #Testes
-df['US 2024 Deficit'] = pd.to_numeric(df['US 2024 Deficit'], errors='coerce')
+df['US 2024 Deficit'] = df['US 2024 Deficit'].str.replace(",", "").astype(float)
 
-
-print(df[['US 2024 Deficit']] > 30)
+print(df[df['US 2024 Deficit'] < -30])
