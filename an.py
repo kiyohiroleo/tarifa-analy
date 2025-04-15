e2fdb9d1bd2271008514798e69eacba523cc5824
kiyohiroleo <kiyohiroleo@gmail.com>
2025-04-15 15:06:20 -0300

    test_column

--- an.py
+++ an.py
@@ -1,5 +1,12 @@
 import pandas as pd
-import numpy
-import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+#Ler o arquivo
+df = pd.read_csv("dataset1.csv", sep=";")
+#Mostra tudo
+pd.set_option('display.max_rows', None)
+#Testes
+df['US 2024 Deficit'] = pd.to_numeric(df['US 2024 Deficit'], errors='coerce')
 
-print("hELOOWORLD")
+
+print(df[['US 2024 Deficit']] > 30)
